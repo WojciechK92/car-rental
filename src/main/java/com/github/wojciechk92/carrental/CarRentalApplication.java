f1af12f958b0b1622f60306d1be9d5f4d311d3f8
“WojciechK92” <wojciechkwitek92@gmail.com>
2024-02-12 12:20:14 +0100

    Initial commit

---
+++ src/main/java/com/github/wojciechk92/carrental/CarRentalApplication.java
@@ -0,0 +1,13 @@
+package com.github.wojciechk92.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CarRentalApplication.class, args);
+  }
+
+}
